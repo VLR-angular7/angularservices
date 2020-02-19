d0f97c94c01b8fc4242f6e804990408f79c02e1d
VLR-angular7 <59470943+VLR-angular7@users.noreply.github.com>
2020-02-19 09:24:40 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,9 @@ export class AppComponent implements OnInit {
   employeeData=[];
   todayDate:Date
   greet:string;
+  allPosts:any;
+  newPost:string;
+  errMsg:string
 
   ngOnInit(){
     this.employeeData=this.empService.getEmpData();
@@ -26,6 +29,13 @@ export class AppComponent implements OnInit {
     console.log("emp data :" +JSON.stringify(this.employeeData));
 
     console.log(this.userSer.msg)
+    this.empService.getAllPosts().subscribe(postsData=>{
+      this.allPosts=postsData;
+      console.log(this.allPosts);
+    },error=>{
+      // alert("Unexpected error occured");
+      this.errMsg="Unexpected Error"
+    })
   
   }
 
@@ -34,5 +44,33 @@ export class AppComponent implements OnInit {
     this.empService.editEmployeeData(data);
 
 
+  }
+
+
+  savePost(){
+// console.log(this.newPost);
+this.empService.savePost(this.newPost).subscribe(res=>{
+  this.allPosts.push(res);
+  // console.log(res);
+})
+  }
+
+  updatePost(post){
+    console.log(post);
+    this.empService.updatePost(post).subscribe(res=>{
+      console.log(res);
+    })
+  }
+
+  deletePost(post){
+    this.empService.deletePost(post).subscribe(res=>{
+      let index=this.allPosts.indexOf(post)
+       this.allPosts.splice(index,1)
+      console.log(res);
+    },(error:Response)=>{
+      if(error.status==404){
+        this.errMsg="Post has been deleted "
+      };
+    })
   }
 }
